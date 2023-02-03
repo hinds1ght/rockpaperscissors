35945e77f927e39420aa8f3a3f9a5a1efbfd86a4
hinds1ght <markgil_yu@yahoo.com>
2023-02-03 17:04:26 +0800

    added best of five function

--- java.js
+++ java.js
@@ -8,25 +8,55 @@ let x = Math.floor(Math.random()*3) + 1;
         return 'scissors';
     }
 }
-function versus(comp, playa){ //hand comparison function
+    let playerScore = 0;
+    let compScore = 0;
+function versus(comp, playa){ //hand comparison function  
     if (comp == 'rock' && playa == 'paper'){
-        return 'computer picked rock, YOU WIN!';
+        playerScore += parseInt(1);
+        console.log("player score :" + playerScore);
+        return 'computer picked rock, you get a point!';   
     }else if (comp == 'rock' && playa == 'scissors'){
-        return 'computer picked rock, YOU LOSE';
+        compScore += parseInt(1);
+        console.log("compscore: " + compScore);
+        return 'computer picked rock, computer gets a point!';
     }else if (comp == 'paper' && playa == 'scissors'){
-        return 'computer picked paper, YOU WIN!';
+        playerScore += parseInt(1);
+        console.log("player score: " + playerScore);
+        return 'computer picked paper, you get a point!';
     }else if (comp == 'paper' && playa == 'rock'){
-        return 'computer picked paper, YOU LOSE!';
+        compScore += parseInt(1);
+        console.log("compscore: " + compScore);
+        return 'computer picked paper, computer gets a point!!';   
     }else if (comp == 'scissors' && playa == 'rock'){
-        return 'computer picked scissors, YOU WIN!';
+        playerScore += parseInt(1);
+        console.log("player score: " + playerScore);
+        return 'computer picked scissors, you get a point!';
     }else if (comp == 'scissors' && playa == 'paper'){
-        return 'computer picked scissors, YOU LOSE!';
+        compScore += parseInt(1);
+        console.log(compScore);
+        return 'computer picked scissors, computer gets a point!!';
     }else if (comp == playa){
         return 'DRAW!';
     }else{
         return 'HUH!?'
     }
 }  
-
-let a = prompt("rock, paper, scissors");
-alert(versus(getComputerChoice(), a.toLowerCase())); //call versus func and display result
+function game(){
+for (i = 0; i < 5; i++){
+    let a = prompt("rock, paper, scissors");
+    alert(versus(getComputerChoice(), a.toLowerCase())); //call versus func and display result
+}
+}
+function whoWon(){
+    if (compScore > playerScore){
+        alert("YOU LOSE! Computer won the game!!!!!");
+        console.log('computer won the game');
+    }else if (compScore < playerScore){
+        alert("YOU WIN! You won the game!!!!!")
+        console.log('you won the game');
+    }else{
+        alert("DRAW!!!!!");
+    }
+}
+game();
+whoWon();
